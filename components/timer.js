df7520d91657140e0dd24cd34701ce85f687bcec
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-08-26 16:02:48 +0200

    create a functional function

--- components/timer.js
+++ components/timer.js
@@ -1,11 +1,55 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 
 export default function Timer(){
-return (
 
+
+    const [seconds,setSeconds] = useState(30)
+    const [minutes, setMinutes] = useState(0)
+
+    const [timerOn, setTimerOn] = useState(false)
+
+    
+
+
+
+
+    useEffect(()=> {
+       const timer = setInterval(() => {
+        if (timerOn) {
+            if(minutes === 0 && seconds === 0) {
+            alert("time up")
+            } else if (seconds === 0) {
+            setMinutes(minutes -1);
+            setSeconds(59);
+            } else { 
+            setSeconds(seconds -1)
+        }} else { clearInterval(timer)}
+
+        },1000)
+        return () => clearInterval(timer)
+      
+        }
+
+    , [minutes, seconds, timerOn]);
+ 
+
+
+return (
+    <>
     <div>
-        <p>hi</p>
+       <span>{minutes<10? "0"+minutes: minutes}</span>
+       <span>:</span>
+       <span>{seconds<10? "0"+seconds: seconds}</span>
+
+       <button type='button' onClick={() => setTimerOn(true)} >START</button>
+       
+       <button type='button' onClick={() => setTimerOn(true)}>RESUME</button>
+
+       <button type='button' onClick={() => setTimerOn(false)} >STOP</button>
+       <button type='button' onClick={() => {setMinutes(1); setSeconds(10)}} >RESET</button>
     </div>
+  
+    </>
 )
 
 }
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Timer from "../components/timer"
 
 export default function Home() {
   return (
-   <h1>hi</h1>
-
-
+      <>
+      <Timer/>
+      </>
    
   )
 }
